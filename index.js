47c846f8f4cc58931cc41ed34ebc3cfe42af0bd0
James Barron <jamesbarron97@gmail.com>
2020-11-01 20:10:51 +0000

    allowing cors

--- index.js
+++ index.js
@@ -2,19 +2,24 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 
-const Boulder = require('./models/boulderModel');
-const boulderRouter = require('./router/boulderRouter')(Boulder);
+const Boulder = require("./models/boulderModel");
+const boulderRouter = require("./router/boulderRouter")(Boulder);
 
 const app = express();
-const db = mongoose.connect('mongodb://18.130.244.7/boulderBucketList', {
+const db = mongoose.connect("mongodb://18.130.244.7/boulderBucketList", {
   useNewUrlParser: true,
-  useUnifiedTopology: true
+  useUnifiedTopology: true,
 });
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
-app.use('/boulders', boulderRouter);
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
+
+app.use("/boulders", boulderRouter);
 // app.use('/notes', noteRoutes);
 
 app.listen(port, () => {
