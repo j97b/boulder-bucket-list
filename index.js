4cbad2e9a3fea9dedd96c188429926363886ae8b
James Barron <jamesbarron97@gmail.com>
2020-11-06 20:58:31 +0000

    fixing res headers

--- index.js
+++ index.js
@@ -17,6 +17,10 @@ app.use(bodyParser.json());
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
   next();
 });
 
